fc618bde04180133530291b53b6d25ce5a405dbc
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2022-12-07 14:30:21 +0100

    MLD

--- creation_table.sql
+++ creation_table.sql
@@ -1,6 +1,6 @@
 -- Creer une base de donnée du nom de "CSGO"
 
-create database CSGO with owner quentin;
+create database CSGO;
 
 DROP TABLE IF EXISTS Nationalites CASCADE;
 DROP TABLE IF EXISTS Joueurs CASCADE;
@@ -23,6 +23,13 @@ CREATE TABLE Matchs (id_match INT NOT NULL, equipe_1 VARCHAR(50), equipe_2 VARCH
 CREATE TABLE Maps (id_map INT NOT NULL, nom_map VARCHAR(50), PRIMARY KEY(id_map));
 
 CREATE TABLE Commentateurs (id_commentateur INT NOT NULL, nom VARCHAR(50), prenom VARCHAR(50), PRIMARY KEY (id_commentateur) );
+
+CREATE TABLE Jouer_match( id_equipe INT, id_equipe_1 INT, id_equipe_2 INT, id_match INT, m_date DATE, PRIMARY KEY(id_equipe, id_equipe_1, id_match),
+   FOREIGN KEY(id_equipe) REFERENCES Equipes(id_equipe),
+   FOREIGN KEY(id_equipe_1) REFERENCES Equipes(id_equipe),
+   FOREIGN KEY(id_equipe_2) REFERENCES Equipes(id_equipe),
+   FOREIGN KEY(id_match) REFERENCES Matchs(id_match));
+
 
 
 
