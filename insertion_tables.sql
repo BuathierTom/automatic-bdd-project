f4df81535c27bd65fc7d52f30d206ab9d2fc36be
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-08 13:56:05 +0100

    nationalité par equipe en commentaire

--- insertion_tables.sql
+++ insertion_tables.sql
@@ -1,38 +1,37 @@
-INSERT INTO Nationalites VALUES(1,'rus');
-INSERT INTO Nationalites VALUES(2,'ukr');
-INSERT INTO Nationalites VALUES(3,'fin');
-INSERT INTO Nationalites VALUES(4,'sue');
-INSERT INTO Nationalites VALUES(5,'usa');
-INSERT INTO Nationalites VALUES(6,'can');
-INSERT INTO Nationalites VALUES(7,'den');
-INSERT INTO Nationalites VALUES(8,'bra');
-INSERT INTO Nationalites VALUES(9,'esp');
-INSERT INTO Nationalites VALUES(10,'fra');
-INSERT INTO Nationalites VALUES(11,'ger');
-INSERT INTO Nationalites VALUES(12,'kos');
-INSERT INTO Nationalites VALUES(13,'ned');
+INSERT INTO Nationalites VALUES(1,'rus');  --team spirit -- Outsiders --
+INSERT INTO Nationalites VALUES(2,'ukr'); -- Natus Vincere (navi) -- 
+INSERT INTO Nationalites VALUES(3,'fin'); -- ENCE -- 
+INSERT INTO Nationalites VALUES(4,'sue'); -- Ninjas in Pyjamas -- 
+INSERT INTO Nationalites VALUES(5,'usa'); -- could9 -- faze Clan --
+INSERT INTO Nationalites VALUES(6,'den'); -- Heroic -- Copenhagen Flames-- 
+INSERT INTO Nationalites VALUES(7,'bra'); -- imperial esports -- furia --
+INSERT INTO Nationalites VALUES(8,'esp'); -- G2 Esport --
+INSERT INTO Nationalites VALUES(9,'fra'); -- Team Vitality --
+INSERT INTO Nationalites VALUES(10,'ger'); -- BIG --
+INSERT INTO Nationalites VALUES(11,'kos'); -- bad new eagle --
+INSERT INTO Nationalites VALUES(12,'ned'); -- team liquid --
 
 
 
-INSERT INTO Equipes VALUES(1,'Team Spirit');
-INSERT INTO Equipes VALUES(2,'Natus Vincere');
-INSERT INTO Equipes VALUES(3,'ENCE');
-INSERT INTO Equipes VALUES(4,'Ninjas in Pyjamas');
-INSERT INTO Equipes VALUES(5,'FaZe Clan');
-INSERT INTO Equipes VALUES(6,'Copenhagen Flames');
-INSERT INTO Equipes VALUES(7,'Heroic');
-INSERT INTO Equipes VALUES(8,'FURIA Esports');
-INSERT INTO Equipes VALUES(9,'G2 Esports');
-INSERT INTO Equipes VALUES(10,'Team Vitality');
-INSERT INTO Equipes VALUES(11,'Imperial Esports');
-INSERT INTO Equipes VALUES(12,'BIG');
-INSERT INTO Equipes VALUES(13,'Cloud9');
-INSERT INTO Equipes VALUES(14,'Outsiders');
-INSERT INTO Equipes VALUES(15,'Bad News Eagles');
-INSERT INTO Equipes VALUES(16,'Team Liquid');
+INSERT INTO Equipes VALUES(1,'Team Spirit'); --
+INSERT INTO Equipes VALUES(2,'Natus Vincere'); --
+INSERT INTO Equipes VALUES(3,'ENCE'); -- 
+INSERT INTO Equipes VALUES(4,'Ninjas in Pyjamas'); -- 
+INSERT INTO Equipes VALUES(5,'FaZe Clan'); --
+INSERT INTO Equipes VALUES(6,'Copenhagen Flames'); -- 
+INSERT INTO Equipes VALUES(7,'Heroic'); --
+INSERT INTO Equipes VALUES(8,'FURIA Esports'); -- 
+INSERT INTO Equipes VALUES(9,'G2 Esports'); -- 
+INSERT INTO Equipes VALUES(10,'Team Vitality'); -- 
+INSERT INTO Equipes VALUES(11,'Imperial Esports'); -- 
+INSERT INTO Equipes VALUES(12,'BIG'); -- 
+INSERT INTO Equipes VALUES(13,'Cloud9'); --
+INSERT INTO Equipes VALUES(14,'Outsiders'); -- 
+INSERT INTO Equipes VALUES(15,'Bad News Eagles'); --
+INSERT INTO Equipes VALUES(16,'Team Liquid'); --
 
 
-INSERT INTO Joueurs VALUES
+INSERT INTO Joueurs VALUES(1,1,1,1)
 
 
 
