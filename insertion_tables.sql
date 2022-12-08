3a7f1abfb666acdae6fed622097480753b751208
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-08 17:32:28 +0100

    un max de joueur mec

--- insertion_tables.sql
+++ insertion_tables.sql
@@ -65,11 +65,32 @@ INSERT INTO Joueurs VALUES(23,5,'2021-01-30',23,5,'Twistzz','Van Dulken','Russel
 INSERT INTO Joueurs VALUES(24,5,'2021-02-15',24,5,'karrigan','Andersen','Finn',32);
 INSERT INTO Joueurs VALUES(25,5,'2022-01-03',25,5,'ropz','Kool','Robin',22);
 
-INSERT INTO Joueurs VALUES(21,5,'2016-01-20',21,5,'rain','Nygaard','Håvard',28);
-INSERT INTO Joueurs VALUES(22,5,'2019-09-26',22,5,'broky','Saukants','Helvijs',21);
-INSERT INTO Joueurs VALUES(23,5,'2021-01-30',23,5,'Twistzz','Van Dulken','Russel',23);
-INSERT INTO Joueurs VALUES(24,5,'2021-02-15',24,5,'karrigan','Andersen','Finn',32);
-INSERT INTO Joueurs VALUES(25,5,'2022-01-03',25,5,'ropz','Kool','Robin',22);
+INSERT INTO Joueurs VALUES(26,6,'2016-01-20',26,6,'rain','Nygaard','Håvard',28);
+INSERT INTO Joueurs VALUES(27,6,'2019-09-26',27,6,'broky','Saukants','Helvijs',21);
+INSERT INTO Joueurs VALUES(28,6,'2021-01-30',28,6,'Twistzz','Van Dulken','Russel',23);
+INSERT INTO Joueurs VALUES(29,6,'2021-02-15',29,6,'karrigan','Andersen','Finn',32);
+INSERT INTO Joueurs VALUES(30,6,'2022-01-03',30,6,'ropz','Kool','Robin',22);
+
+INSERT INTO Joueurs VALUES(31,7,'2022-08-13',31,7,'birdfromsky','Due Frederiksen','Thomas',29);
+INSERT INTO Joueurs VALUES(32,7,'2022-08-13',32,7,'regali','Harjău','Iulian',20);
+INSERT INTO Joueurs VALUES(33,7,'2022-08-13',33,7,'raalz','Steensborg','Rasmus',27);
+INSERT INTO Joueurs VALUES(34,7,'2022-08-13',34,7,'TMB','Bundsbæk','Thomas',20);
+INSERT INTO Joueurs VALUES(35,7,'2022-08-13',35,7,'b0RUP','Borup','Johannes',23);
+
+INSERT INTO Joueurs VALUES(36,8,'2020-04-05',36,8,'stavn','Lund','Martin',20);
+INSERT INTO Joueurs VALUES(37,8,'2020-04-05',37,8,'cadiaN','Møller','Casper',27);
+INSERT INTO Joueurs VALUES(38,8,'2020-04-08',38,8,'TeSeS','Madsen','René',21);
+INSERT INTO Joueurs VALUES(39,8,'2021-02-26',39,8,'sjuush','Beck','Rasmus',23);
+INSERT INTO Joueurs VALUES(40,8,'2022-06-21',40,8,'Jabbi','Nygaard','Jakob',19);
+
+INSERT INTO Joueurs VALUES(41,9,'2017-11-08',41,9,'yuurih','Boian','Yuri',22);
+INSERT INTO Joueurs VALUES(42,9,'2018-02-06',42,9,'arT','Piovezan','Andrei',26);
+INSERT INTO Joueurs VALUES(43,9,'2018-02-06',43,9,'KSCERATO','Cerato','Kaike',23);
+INSERT INTO Joueurs VALUES(44,9,'2021-08-23',44,9,'drop','Abreu','André',18);
+INSERT INTO Joueurs VALUES(45,9,'2022-01-06',45,9,'saffee','Costa','Rafael',27);
+
+
+
 
 
 
