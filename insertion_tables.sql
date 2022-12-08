b791fe332e9dbd326566096f350eb5e5613c395c
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-08 16:37:57 +0100

    bon pour MLD

--- insertion_tables.sql
+++ insertion_tables.sql
@@ -41,6 +41,12 @@ INSERT INTO Joueurs VALUES(3,1,'2022-02-06',3,1,'Robert','Isyanov','Patsi',19);
 INSERT INTO Joueurs VALUES(4,1,'2022-02-06',4,1,'Pavel','Ogloblin','s1ren',20);
 INSERT INTO Joueurs VALUES(5,1,'2022-06-21',5,1,'Ihor','Zhdanov','w0nderful	',17);
 
+INSERT INTO Joueurs VALUES(6,2,'2016-08-04',6,2,'s1mple','Oleksandr','Kostyliev	',25);
+INSERT INTO Joueurs VALUES(7,2,'2017-11-06',7,2,'electroNic','Sharipov','Denis',24);
+INSERT INTO Joueurs VALUES(8,2,'2020-01-24',8,2,'Perfecto','Zalutskiy','Ilya',23);
+INSERT INTO Joueurs VALUES(9,2,'2020-12-20',9,2,'b1t','Valerii','Vakhovskyi',19);
+INSERT INTO Joueurs VALUES(10,2,'2020-12-20',10,2,'b1t','Valerii','Vakhovskyi',19);
+
 
 
 
@@ -48,9 +54,21 @@ INSERT INTO Joueurs VALUES(5,1,'2022-06-21',5,1,'Ihor','Zhdanov','w0nderful	',17
 
 INSERT INTO Stats VALUES(1, 2550, 256, 546);
 
+-- id match -- id commentateur -- id map -- scores --
 
+INSERT INTO Matchs VALUES(1,1,1,'16/5');
 
+-- id map -- nom map --
 
+INSERT INTO Maps VALUES(1,'Dust 2');
+
+-- id comment -- nom -- prenom --
+
+INSERT INTO Commentateurs VALUES(1,'test','test');
+
+-- id equipe -- id equipe 1 -- id equipe 2 -- id match -- date --
+
+INSERT INTO Jouer_match VALUES(1,1,5,1,'2022-12-08');
 
 
 
