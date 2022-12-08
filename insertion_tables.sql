20d5db83f5f8c860ed7d40083908bafc2b2a3203
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-08 14:00:39 +0100

    changement du nom prenom de joueur en pseudo de joueur

--- insertion_tables.sql
+++ insertion_tables.sql
@@ -1,3 +1,5 @@
+-- id nationalité -- lib nationalité --
+
 INSERT INTO Nationalites VALUES(1,'rus');  --team spirit -- Outsiders --
 INSERT INTO Nationalites VALUES(2,'ukr'); -- Natus Vincere (navi) -- 
 INSERT INTO Nationalites VALUES(3,'fin'); -- ENCE -- 
@@ -12,6 +14,7 @@ INSERT INTO Nationalites VALUES(11,'kos'); -- bad new eagle --
 INSERT INTO Nationalites VALUES(12,'ned'); -- team liquid --
 
 
+-- id equipe -- nom d'equipe --
 
 INSERT INTO Equipes VALUES(1,'Team Spirit'); --
 INSERT INTO Equipes VALUES(2,'Natus Vincere'); --
@@ -30,6 +33,7 @@ INSERT INTO Equipes VALUES(14,'Outsiders'); --
 INSERT INTO Equipes VALUES(15,'Bad News Eagles'); --
 INSERT INTO Equipes VALUES(16,'Team Liquid'); --
 
+-- id personne -- id equipe -- id stat -- id nationalité --  pseudo -- date nai --
 
 INSERT INTO Joueurs VALUES(1,1,1,1)
 
