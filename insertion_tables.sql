9bef8e84b3dedd3436ac6ae1603d21d4a0c75f9b
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-08 13:41:12 +0100

    oui

---
+++ insertion_tables.sql
@@ -0,0 +1,86 @@
+INSERT INTO Nationalites VALUES(1,'rus');
+INSERT INTO Nationalites VALUES(2,'ukr');
+INSERT INTO Nationalites VALUES(3,'fin');
+INSERT INTO Nationalites VALUES(4,'sue');
+INSERT INTO Nationalites VALUES(5,'usa');
+INSERT INTO Nationalites VALUES(6,'can');
+INSERT INTO Nationalites VALUES(7,'den');
+INSERT INTO Nationalites VALUES(8,'bra');
+INSERT INTO Nationalites VALUES(9,'esp');
+INSERT INTO Nationalites VALUES(10,'fra');
+INSERT INTO Nationalites VALUES(11,'ger');
+INSERT INTO Nationalites VALUES(12,'kos');
+INSERT INTO Nationalites VALUES(13,'ned');
+
+
+
+INSERT INTO Equipes VALUES(1,'Team Spirit');
+INSERT INTO Equipes VALUES(2,'Natus Vincere');
+INSERT INTO Equipes VALUES(3,'ENCE');
+INSERT INTO Equipes VALUES(4,'Ninjas in Pyjamas');
+INSERT INTO Equipes VALUES(5,'FaZe Clan');
+INSERT INTO Equipes VALUES(6,'Copenhagen Flames');
+INSERT INTO Equipes VALUES(7,'Heroic');
+INSERT INTO Equipes VALUES(8,'FURIA Esports');
+INSERT INTO Equipes VALUES(9,'G2 Esports');
+INSERT INTO Equipes VALUES(10,'Team Vitality');
+INSERT INTO Equipes VALUES(11,'Imperial Esports');
+INSERT INTO Equipes VALUES(12,'BIG');
+INSERT INTO Equipes VALUES(13,'Cloud9');
+INSERT INTO Equipes VALUES(14,'Outsiders');
+INSERT INTO Equipes VALUES(15,'Bad News Eagles');
+INSERT INTO Equipes VALUES(16,'Team Liquid');
+
+
+INSERT INTO Joueurs VALUES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
