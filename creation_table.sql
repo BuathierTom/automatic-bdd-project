473b593e53d58453e9d1b20df39b14a0ad36ae3d
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-08 14:41:14 +0100

    ajout du nom et prenom et inserstion des joueurs de la premiere equipe

--- creation_table.sql
+++ creation_table.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS Jouer_match CASCADE;
 
 CREATE TABLE Nationalites (id_nationalite INT NOT NULL, lib_nationalite VARCHAR(3), PRIMARY KEY (id_nationalite) );
 
-CREATE TABLE Joueurs (id_personne INT NOT NULL,id_equipe INT NOT NULL, date_join DATE,id_stat INT NOT NULL, id_nationalite INT NOT NULL, pseudo VARCHAR(50), age INT, PRIMARY KEY (id_personne) );
+CREATE TABLE Joueurs (id_personne INT NOT NULL,id_equipe INT NOT NULL, date_join DATE,id_stat INT NOT NULL, id_nationalite INT NOT NULL,nom VARCHAR(50), prenom VARCHAR(50), pseudo VARCHAR(50), age INT, PRIMARY KEY (id_personne) );
 
 CREATE TABLE Stats (id_stat INT NOT NULL, s_kill INT, death INT, assist INT, PRIMARY KEY (id_stat) );
 
--- insertion_tables.sql
+++ insertion_tables.sql
@@ -16,26 +16,38 @@ INSERT INTO Nationalites VALUES(12,'ned'); -- team liquid --
 
 -- id equipe -- nom d'equipe --
 
-INSERT INTO Equipes VALUES(1,'Team Spirit'); --
-INSERT INTO Equipes VALUES(2,'Natus Vincere'); --
-INSERT INTO Equipes VALUES(3,'ENCE'); -- 
-INSERT INTO Equipes VALUES(4,'Ninjas in Pyjamas'); -- 
-INSERT INTO Equipes VALUES(5,'FaZe Clan'); --
-INSERT INTO Equipes VALUES(6,'Copenhagen Flames'); -- 
-INSERT INTO Equipes VALUES(7,'Heroic'); --
-INSERT INTO Equipes VALUES(8,'FURIA Esports'); -- 
-INSERT INTO Equipes VALUES(9,'G2 Esports'); -- 
-INSERT INTO Equipes VALUES(10,'Team Vitality'); -- 
-INSERT INTO Equipes VALUES(11,'Imperial Esports'); -- 
-INSERT INTO Equipes VALUES(12,'BIG'); -- 
-INSERT INTO Equipes VALUES(13,'Cloud9'); --
-INSERT INTO Equipes VALUES(14,'Outsiders'); -- 
-INSERT INTO Equipes VALUES(15,'Bad News Eagles'); --
-INSERT INTO Equipes VALUES(16,'Team Liquid'); --
+INSERT INTO Equipes VALUES(1,'Team Spirit');
+INSERT INTO Equipes VALUES(2,'Natus Vincere'); 
+INSERT INTO Equipes VALUES(3,'ENCE'); 
+INSERT INTO Equipes VALUES(4,'Ninjas in Pyjamas'); 
+INSERT INTO Equipes VALUES(5,'FaZe Clan'); 
+INSERT INTO Equipes VALUES(6,'Copenhagen Flames'); 
+INSERT INTO Equipes VALUES(7,'Heroic'); 
+INSERT INTO Equipes VALUES(8,'FURIA Esports'); 
+INSERT INTO Equipes VALUES(9,'G2 Esports'); 
+INSERT INTO Equipes VALUES(10,'Team Vitality'); 
+INSERT INTO Equipes VALUES(11,'Imperial Esports'); 
+INSERT INTO Equipes VALUES(12,'BIG');
+INSERT INTO Equipes VALUES(13,'Cloud9');
+INSERT INTO Equipes VALUES(14,'Outsiders'); 
+INSERT INTO Equipes VALUES(15,'Bad News Eagles');
+INSERT INTO Equipes VALUES(16,'Team Liquid');
 
--- id personne -- id equipe -- id stat -- id nationalité --  pseudo -- date nai --
+--id personne--id equipe--date join--id stat--id nationalité--nom--prenom--pseudo--age--
 
-INSERT INTO Joueurs VALUES(1,1,1,1,)
+INSERT INTO Joueurs VALUES(1,1,'2019-03-03',1,1,'Leonid','Vishnyakov','chopper',25);
+INSERT INTO Joueurs VALUES(2,1,'2019-09-26',2,1,'Boris','Vorobyev','magixx',19);
+INSERT INTO Joueurs VALUES(3,1,'2022-02-06',3,1,'Robert','Isyanov','Patsi',19);
+INSERT INTO Joueurs VALUES(4,1,'2022-02-06',4,1,'Pavel','Ogloblin','s1ren',20);
+INSERT INTO Joueurs VALUES(5,1,'2022-06-21',5,1,'Ihor','Zhdanov','w0nderful	',17);
+
+
+
+
+-- id stat -- kill --  death --  assist --
+
+INSERT INTO Stats VALUES(1, 2550, 256, 546);
+
 
 
 
