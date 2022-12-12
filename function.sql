71733c0a5ee58753e73ac8ca978c02651546d0cf
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2022-12-12 17:00:47 +0100

    Tqt

--- function.sql
+++ function.sql
@@ -1,13 +1,14 @@
 -- Fonction qui return les noms des joueurs en fonction du nom de l'equipe données
 
-CREATE OR REPLACE FUNCTION select_equipe(nom_E)
+CREATE OR REPLACE FUNCTION select_equipe(nom_E Equipes%ROWTYPE)
 RETURNS void AS $$
 DECLARE
-    nom_joueur, prenom_joueur VARCHAR;
+    nom_joueur Joueurs%ROWTYPE;
+    prenom_joueur Joueurs%ROWTYPE;
 BEGIN
     SELECT nom, prenom INTO nom_joueur, prenom_joueur FROM Joueurs as j
         INNER JOIN Equipes AS e ON j.id_equipe = e.id_equipe
-            WHERE nom_E = nom_equipe;
+            WHERE nom_equipe = nom_E;
 
 END;
 LANGUAGE plpgsql;
