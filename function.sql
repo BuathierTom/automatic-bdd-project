9f7e7ac03af5017b35a578a7288a63264dd44ee3
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-13 16:00:18 +0100

    creation de 3 fonctions

--- function.sql
+++ function.sql
@@ -19,15 +19,43 @@ BEGIN
     raise notice 'nom: %, | prenom: %, | pseudo : %',nom_joueur, prenom_joueur, pseudo_joueur;
     end loop;
 
-    
+
 END;
 $$LANGUAGE plpgsql;
 
 
 select select_equipe('BIG');
 
--- Fonction qui retourne le raport entre le nombre d'ennemie tué et le nombre de mort qu'a chaque joueur
 
+-- premiere fonction d'affichage du ratio tué / mort 
+
+CREATE OR REPLACE FUNCTION affiche_kd_joueur()
+RETURNS void as $$
+DECLARE
+
+    donnee record;
+    
+    v_kd real;
+    v_kill real;
+    v_death real;
+
+BEGIN
+    
+    
+    for donnee in select pseudo, s_kill, death  from Stats as S inner join Joueurs as J on S.id_stat = J.id_personne
+
+    loop
+    v_kd = donnee.s_kill/donnee.death;
+    raise notice 'le kd de % est de % ',donnee.pseudo,v_kd;
+    end loop;
+
+end;
+$$ LANGUAGE plpgsql;
+
+select affiche_kd_joueur();
+
+
+-- Fonction qui retourne le raport entre le nombre d'ennemie tué et le nombre de mort qu'a chaque joueur
 
 CREATE OR REPLACE FUNCTION affiche_kd_joueur()
 RETURNS void as $$
@@ -47,12 +75,46 @@ BEGIN
     inner join Joueurs as J on S.id_stat = J.id_personne
     offset i-1 limit i;
     v_kd := v_kill/v_death;
-    raise notice 'kill -> % | mort -> %', v_kill , v_death;
     raise notice 'le kd de % est de % ', v_pseudo, v_kd;
     end loop;
 
 end;
 $$ LANGUAGE plpgsql;
+
+select affiche_kd_joueur();
+
+
+-- fonction qui retourne le kd de chaque joueur mais avec un curseur
+
+CREATE OR REPLACE FUNCTION affiche_kd_joueur()
+RETURNS void as $$
+DECLARE
+    mon_curseur cursor for select  pseudo, s_kill, death from Stats as S inner join Joueurs as J on S.id_stat = J.id_personne;
+    v_pseudo Joueurs.pseudo%type;
+    v_kd real;
+    v_kill real;
+    v_death real;
+
+BEGIN
+
+    raise notice 'voici le pseudo et le kd des joueurs :';
+
+    open mon_curseur;
+    loop
+    fetch mon_curseur into v_pseudo, v_kill, v_death;
+    exit when not found;
+    v_kd := v_kill/v_death;
+    raise notice 'le kd de % est de % ', v_pseudo, v_kd;
+    end loop;
+    close mon_curseur;
+
+end;
+$$ LANGUAGE plpgsql;
+
+select affiche_kd_joueur();
+
+
+
 
 
 
