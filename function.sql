d3058d2a24616b55663b601ee1898d2ba434d519
QuentinGilleron <quentin.gilleron5627@gmail.com>
2022-12-15 14:12:42 +0100

    stiti

--- function.sql
+++ function.sql
@@ -20,15 +20,38 @@ BEGIN
     exit when not found;
     raise notice 'nom: %, | prenom: %, | pseudo : %',nom_joueur, prenom_joueur, pseudo_joueur;
     end loop;
+    close mon_curseur;
 
 
 END;
 $$LANGUAGE plpgsql;
 
-
 select select_equipe('BIG');
 
 
+-- fonction utilisant un curseur qui permet de classé les équipes dans l'ordre de leur nombre de victoires, du plus au moins victorieux
+Create or replace function classement_equipe()
+returns void as $$
+DECLARE
+    mon_curseur cursor for select nom_equipe from Equipes order by equipes.nb_vitoire desc;
+    v_classement Equipes.nom_equipe%type;
+    i integer;
+BEGIN
+
+    i := 1;
+    open mon_curseur;
+    loop
+    fetch mon_curseur into v_classement;
+    exit when not found;
+    raise notice '% est numero %',v_classement,i;
+    i := i + 1;
+    end loop;
+    close mon_curseur;
+
+end;
+$$ language plpgsql;
+
+select classement_equipe();
 
 
 
